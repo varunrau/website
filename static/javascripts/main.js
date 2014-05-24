29daf8543acb1a99a2028264403a6f376cf63369
varunrau <varunrau@gmail.com>
2014-05-24 12:43:47 -0400

    added owl carousel

---
+++ static/javascripts/main.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  items: 1,
+  center: true,
+  loop: true,
+  margin: 10000,
+  lazyLoad: true,
+  merge: true,
+  video: true,
+  freeDrag: true,
+  responsive:{
+    480:{
+      items:2
+    },
+
+    678:{
+      items:3
+    },
+
+    960:{
+      items:3
+    }
+  }
+});
